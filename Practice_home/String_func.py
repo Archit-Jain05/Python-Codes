0956b73253bfe10e3dce0b7c178efaf44a7d58d9
Archit-Jain05 <architjain2005@gmail.com>
2023-03-13 19:21:15 +0530

    Create String_func.py

---
+++ Practice_home/String_func.py
@@ -0,0 +1,24 @@
+a="Hello world"
+
+'''len()'''
+#l=len(a)
+#print(l)
+
+'''capitalise()'''
+#print(a.capitalize())
+
+'''casefold()'''
+#print(a.casefold())
+
+'''lower()'''
+#print(a.lower())
+
+'''swapcase()'''
+#print(a.swapcase())
+
+'''title()'''
+#print(a.title())
+
+'''replace()'''
+a.replace('w','H')
+print(a)
