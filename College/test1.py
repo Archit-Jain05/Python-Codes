bb161f242121c7ee3b48d8e35f7efc9761abfe02
Archit-Jain05 <architjain2005@gmail.com>
2023-04-28 10:00:20 +0530

    hi

---
+++ College/test1.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+
+  user="root",
+
+  password="",
+  database="college",
+
+)
+
+if mydb:
+
+  print("Connection Established Successfully")
+
+else:
+
+  print("Connection Error")
