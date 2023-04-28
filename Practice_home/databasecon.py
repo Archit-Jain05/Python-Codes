7e8e3e8841aa03f22c75b7c99cd2eefa4666254a
Archit-Jain05 <architjain2005@gmail.com>
2023-04-28 15:18:23 +0530

    1

---
+++ Practice_home/databasecon.py
@@ -0,0 +1,11 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="college"
+)
+if mydb:
+  print("Connection Established Successfully")
+else:
+  print("Connection Error")
--- Practice_home/tkinter_prac.py
+++ Practice_home/tkinter_prac.py
@@ -3,6 +3,8 @@ import tkinter as tk
 m=tk.Tk()
 
 b1=tk.Button(m,text='Hello world',width=15,height=10)
+l=tk.Entry(m,{},width=40)
 b1.pack()
+l.pack()
 m.title('Trial')
 m.mainloop()
